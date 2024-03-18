f3270a64e3ac7f80d9a5850e03b9aa82d9c6fa8e
christian <christ.heaven22@gmail.com>
2024-03-18 23:42:02 +0700

    create middleware response + request

---
+++ common-library/Base/Helper.cs
@@ -0,0 +1,28 @@
+﻿using common_library.Base.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common_library.Base
+{
+    public static class Helper
+    {
+        public static CommonResponse ResponseWrapper(object? result, HttpContext context, object? exception = null)
+        {
+            var requestUrl = context.Request.GetDisplayUrl();
+            var data = result;
+            var error = exception != null ? "Internal Server Error" : null;
+            var status = result != null && (HttpStatusCode)context.Response.StatusCode == HttpStatusCode.OK;
+            var httpStatusCode = (HttpStatusCode)context.Response.StatusCode;
+
+
+            return new CommonResponse(requestUrl, data, error, status, httpStatusCode);
+        }
+
+    }
+}
